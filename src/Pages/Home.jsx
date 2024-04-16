b498ef754571730a28f96ee183ea323c6f33fa57
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-04-16 22:22:59 +0600

    helmet is used

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import Helmet from "react-helmet";
 import CustomerReviews from "../Components/CustomerReviews";
 import FAQ from "../Components/FAQ";
 import Slider from "../Components/Slider";
@@ -6,6 +7,9 @@ import TopProperties from "../Components/TopProperties";
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home</title>
+            </Helmet>
             <Slider></Slider>
             <TopProperties></TopProperties>
             <CustomerReviews></CustomerReviews>
